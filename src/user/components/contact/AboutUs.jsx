083de1b2b1520f778e5addf1e624de5ab5152e79
seonekr <145427113+seonekr@users.noreply.github.com>
2024-05-08 17:20:56 +0700

    create introduce page

--- src/user/components/contact/AboutUs.jsx
+++ src/user/components/contact/AboutUs.jsx
@@ -6,44 +6,41 @@ import grl from "./officegrl.jpg";
 const AboutUs = () => {
   return (
     <div className="about-us" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="box-1">
-        <div class="text-area">
-          <div class="pic">
-            <img src={grl} alt="" />
-          </div>
-          <p class="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-            repudiandae aut vero laboriosam accusantium esse ipsa nulla
-            molestias facilis tempore. Culpa nam aut officiis totam quasi
-            inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Vel repudiandae aut vero laboriosam accusantium
-            esse ipsa nulla molestias facilis tempore. Culpa nam aut officiis
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-            repudiandae aut vero laboriosam accusantium esse ipsa nulla
-            molestias facilis tempore. Culpa nam aut officiis totam quasi
-            inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Vel repudiandae aut vero laboriosam accusantium
-            esse ipsa nulla molestias facilis tempore. Culpa nam aut officiis
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-            repudiandae aut vero laboriosam accusantium esse ipsa nulla
-            molestias facilis tempore. Culpa nam aut officiis totam quasi
-            inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Vel repudiandae aut vero laboriosam accusantium
-            esse ipsa nulla molestias facilis tempore. Culpa nam aut officiis
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-            repudiandae aut vero laboriosam accusantium esse ipsa nulla
-            molestias facilis tempore. Culpa nam aut officiis totam quasi
-            inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Vel repudiandae aut vero laboriosam accusantium
-            esse ipsa nulla molestias facilis tempore. Culpa nam aut officiis
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-            repudiandae aut vero laboriosam accusantium esse ipsa nulla
-            molestias facilis tempore. Culpa nam aut officiis totam quasi
-            inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Vel repudiandae aut vero laboriosam accusantium
-            esse ipsa nulla molestias facilis tempore. Culpa nam aut officiis
-          </p>
+      <div class="box-1">
+        <div class="pic">
+          <img src={grl} alt="" />
         </div>
+        <p class="text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+          repudiandae aut vero laboriosam accusantium esse ipsa nulla molestias
+          facilis tempore. Culpa nam aut officiis totam quasi inventore ullam
+          dolorum? Eum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Vel repudiandae aut vero laboriosam accusantium esse ipsa nulla
+          molestias facilis tempore. Culpa nam aut officiis Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Vel repudiandae aut vero
+          laboriosam accusantium esse ipsa nulla molestias facilis tempore.
+          Culpa nam aut officiis totam quasi inventore ullam dolorum? Eum? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Vel repudiandae aut
+          vero laboriosam accusantium esse ipsa nulla molestias facilis tempore.
+          Culpa nam aut officiis Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Vel repudiandae aut vero laboriosam accusantium esse
+          ipsa nulla molestias facilis tempore. Culpa nam aut officiis totam
+          quasi inventore ullam dolorum? Eum? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Vel repudiandae aut vero laboriosam
+          accusantium esse ipsa nulla molestias facilis tempore. Culpa nam aut
+          officiis Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+          repudiandae aut vero laboriosam accusantium esse ipsa nulla molestias
+          facilis tempore. Culpa nam aut officiis totam quasi inventore ullam
+          dolorum? Eum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Vel repudiandae aut vero laboriosam accusantium esse ipsa nulla
+          molestias facilis tempore. Culpa nam aut officiis Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Vel repudiandae aut vero
+          laboriosam accusantium esse ipsa nulla molestias facilis tempore.
+          Culpa nam aut officiis totam quasi inventore ullam dolorum? Eum? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Vel repudiandae aut
+          vero laboriosam accusantium esse ipsa nulla molestias facilis tempore.
+          Culpa nam aut officiis
+        </p>
       </div>
     </div>
   );
