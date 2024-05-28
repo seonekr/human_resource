1eb7ca37fae48863691092942f281d67146e61b0
seonekr <145427113+seonekr@users.noreply.github.com>
2024-05-28 09:16:09 +0700

    about us

--- src/user/components/contact/Introduce1.jsx
+++ src/user/components/contact/Introduce1.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./aboutUs.css";
 
 const Introduce1 = () => {
+
+  const [pdf, setPDF] = useState([])
+
+
+  useEffect(() => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: import.meta.env.VITE_API + `company/webinfo/list`,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        setPDF(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  console.log(pdf)
 
   return (
     <>
